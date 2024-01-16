d2ef477b61b787c5e200e08cfc4669c86bc45601
Tina Wang <tw1720@ic.ac.uk>
2024-01-16 15:02:21 +0000

    Fetch api keys from the .env file

--- db_utils.py
+++ db_utils.py
@@ -1,11 +1,19 @@
 import requests
+import os
+from dotenv import load_dotenv
 
 url = "https://semadb.p.rapidapi.com/collections"
 
-print("${RAPID_API_KEY}")
+load_dotenv()
+
+RAPID_API_KEY = os.getenv('RAPID_API_KEY')
+RAPID_API_HOST = os.getenv('RAPID_API_HOST')
+
+print(RAPID_API_KEY)
+print(RAPID_API_HOST)
 headers = {
-    "X-RapidAPI-Key": "${RAPID_API_KEY}",
-    "X-RapidAPI-Host": "${RAPID_API_HOST}"
+    "X-RapidAPI-Key": f"{RAPID_API_KEY}",
+    "X-RapidAPI-Host": f"{RAPID_API_HOST}"
 }
 
 response = requests.get(url, headers=headers)
--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # RAPID API
-echo RAPID_API_KEY=$RAPID_API_KEY >> .env
-echo RAPID_API_HOST=$RAPID_API_KEY >> .env
+echo RAPID_API_KEY="$RAPID_API_KEY" >> .env
+echo RAPID_API_HOST="$RAPID_API_KEY" >> .env
