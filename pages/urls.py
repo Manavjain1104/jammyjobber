d9e3766227ee1eedf1ed588be4c18c5efabc7943
Krish Maha <krish@mahas.net>
2024-01-17 13:51:46 +0000

    Set up hello world django app

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-from .views import home_page_view       #, submitted_search_view
-from .views import job_search
+from .views import home_page_view
 
 urlpatterns = [
-    path("", home_page_view, name="search"),
-]
-
-# path("search/", submitted_search_view, name="search"),
+    path("", home_page_view, name="home"),]
