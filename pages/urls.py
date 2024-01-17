8213508880841c3de61ed84c8344c9e6c72af16d
Krish Maha <krish@mahas.net>
2024-01-17 15:51:08 +0000

    Add search box to page

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home_page_view
+from .views import home_page_view, submitted_search_view
 
 urlpatterns = [
-    path("", home_page_view, name="home"),]
+    path("", home_page_view, name="search"),
+    path("search/", submitted_search_view, name="search")]
