16fd8a955073ced7f85fbdf2fd5590c3fa3571ef
Tina Wang <tw1720@ic.ac.uk>
2024-01-18 21:27:58 +0000

    Give the query search result in the webpage

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_page_view, submitted_search_view, my_function
+from .views import home_page_view, submitted_search_view
 
 urlpatterns = [
     path("", home_page_view, name="search"),
