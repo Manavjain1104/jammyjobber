588b8a052883e524754a9a1a7d8393de5983e293
Tina Wang <tw1720@ic.ac.uk>
2024-01-18 15:13:39 +0000

    Add a button that runs predefined embeddings

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_page_view, submitted_search_view
+from .views import home_page_view, submitted_search_view, my_function
 
 urlpatterns = [
     path("", home_page_view, name="search"),
