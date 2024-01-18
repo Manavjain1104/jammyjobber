8fb018fcd8703c0e315fbaf90895061a39df6b3a
Tina Wang <tw1720@ic.ac.uk>
2024-01-18 15:13:39 +0000

    Add a button that runs predefined embeddings

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home_page_view, submitted_search_view
+from .views import home_page_view, submitted_search_view, my_function
 
 urlpatterns = [
     path("", home_page_view, name="search"),
-    path("search/", submitted_search_view, name="search")]
+    path("search/", submitted_search_view, name="search"),
+    path('my_function/', my_function, name='my_function'),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from django.shortcuts import render
 from utils.semadb_utils import *
 from utils.llm_utils import *
@@ -105,3 +106,37 @@ def job_search(request):
 
     return render(request, 'pages/home_search.html', {'job_list': job_list})
 
+=======
+from django.shortcuts import render
+from django.http import HttpResponse
+from semadb_utils import *
+from embeddings_utils import *
+
+# Create your views here.
+def home_page_view(request):
+    return render(request, "pages/home_search.html", {})
+
+
+def submitted_search_view(request):
+    return render(request, "pages/submitted_search.html", {})
+
+
+def my_view(request):
+    return render(request, 'my_template.html')
+
+
+def my_function(request):
+    # Your function logic here
+    jobs = ["This is a Software Engineering job.", "You will be a receptionist at our clinic.",
+            "We are looking for a python developer. We can offer a competetive salary", "Charity worker needed.",
+            "Dog sitter job in Manchester"]
+
+    job = "Only London"
+
+    request_embedding = create_embedding(job)
+    closest = search_points("testJobs", request_embedding, 1)
+
+    print(jobs[closest[0]])
+
+    return HttpResponse("Function called successfully!")
+>>>>>>> 96da412 (Add a button that runs predefined embeddings)
