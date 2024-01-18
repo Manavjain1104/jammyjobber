29536a1c366c39b411ec661eca4bdbd824f4addd
Tina Wang <tw1720@ic.ac.uk>
2024-01-18 15:13:39 +0000

    Add a button that runs predefined embeddings

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from semadb_utils import *
+from embeddings_utils import *
 
 # Create your views here.
 def home_page_view(request):
@@ -8,3 +10,23 @@ def home_page_view(request):
 
 def submitted_search_view(request):
     return render(request, "pages/submitted_search.html", {})
+
+
+def my_view(request):
+    return render(request, 'my_template.html')
+
+
+def my_function(request):
+    # Your function logic here
+    jobs = ["This is a Software Engineering job.", "You will be a receptionist at our clinic.",
+            "We are looking for a python developer. We can offer a competetive salary", "Charity worker needed.",
+            "Dog sitter job in Manchester"]
+
+    job = "Only London"
+
+    request_embedding = create_embedding(job)
+    closest = search_points("testJobs", request_embedding, 1)
+
+    print(jobs[closest[0]])
+
+    return HttpResponse("Function called successfully!")
