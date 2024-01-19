8771a1bd374d8877f33adf8ed0442a25d866a7dd
christieszi <kristi060802@gmail.com>
2024-01-19 16:34:33 +0000

    Try test url

---
+++ test/test_db.py
@@ -0,0 +1,6 @@
+from semadb_utils import *
+
+def test_url_generation():
+    assert collection_url("test_collection") == "https://semadb.p.rapidapi.com/collections/test_collection"
+
+test_url_generation()
