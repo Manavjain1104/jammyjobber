704dca1f2cdee37db6bdb5fb5f89652f50674ad0
christieszi <kristi060802@gmail.com>
2024-01-19 16:37:42 +0000

    Try change import

--- test/test_db.py
+++ test/test_db.py
@@ -1,4 +1,4 @@
-from semadb_utils import *
+from ../semadb_utils import *
 
 def test_url_generation():
     assert collection_url("test_collection") == "https://semadb.p.rapidapi.com/collections/test_collection"
