c0d322b29ee96e83221353a69d941bfe1898c6dc
christieszi <kristi060802@gmail.com>
2024-01-19 16:59:47 +0000

    Reset module path

--- test/test_db.py
+++ test/test_db.py
@@ -1,6 +1,4 @@
-import sys
-sys.path.append('/utils')
-import semadb_utils as semadb
+import utils.semadb_utils as semadb
 
 def test_url_generation():
     assert semadb.collection_url("test_collection") == "https://semadb.p.rapidapi.com/collections/test_collection"
