9d0212ea972adc9668c4eadbfae38588d1811aeb
christieszi <kristi060802@gmail.com>
2024-01-22 13:35:54 +0000

    Add tests for url generation

--- test/test_db.py
+++ test/test_db.py
@@ -3,5 +3,8 @@ import utils.semadb_utils as semadb
 
 def test_url_generation():
     assert semadb.collection_url("test_collection") == "https://semadb.p.rapidapi.com/collections/test_collection"
+    assert semadb.points_url("test_collection") == "https://semadb.p.rapidapi.com/collections/test_collection/points"
+    assert semadb.search_url("test_collection") == "https://semadb.p.rapidapi.com/collections/test_collection/points/search"
+    assert semadb.points_url("") == "https://semadb.p.rapidapi.com/collections//points"
 
 test_url_generation()
