a832859774fad326384ba60b534566989b0ae09b
Tina Wang <tw1720@ic.ac.uk>
2024-01-25 12:59:27 +0000

    Add show description button

--- pages/models.py
+++ pages/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class Job(models.Model):
+    job_id = models.PositiveIntegerField()
     title = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
-    # description = models.TextField()
+    location = models.CharField(max_length=255)
+    description = models.TextField()
     link = models.CharField(max_length=255)
 
     def __str__(self):
