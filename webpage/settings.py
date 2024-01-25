055097d864c326f5b87f16ee25f5d15a88378386
Jain, Manav <manav.jain21@imperial.ac.uk>
2024-01-25 01:17:02 +0000

    Update settings.py

--- webpage/settings.py
+++ webpage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-z93nc61!^9yfltx1b)!wuiy9#7nahwi-@qn#6p_0fdezy=fygm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jammyjobber-simplewebapp-977d4fb51203.herokuapp.com','198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
