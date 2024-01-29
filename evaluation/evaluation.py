839a50f726f4d5d611106c0762a7fd50c646d95d
Tina Wang <tw1720@ic.ac.uk>
2024-01-29 13:43:49 +0000

    Add csv from the evaluation and basic template for the evaluation function

---
+++ evaluation/evaluation.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+# ------- HELPER --------
+
+
+def csv_to_sqlite(path_to_csv):
+    """Convert the information from csv to sqlite
+
+    Args:
+        path_to_csv (str): path to the csv file
+    """
+    ...
+
+
+# ------- Evaluation --------
+def top_n_accuracy():
+    ...
+
+
+def hit_rate():
+    ...
+
+
+if __name__ == "__main__":
+    top_n_accuracy()
