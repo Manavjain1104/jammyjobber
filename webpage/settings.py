f61db6d5f9fa3a7bc050a8615926e1bbab1a270b
shrutip2003 <pradhan2003shruti@gmail.com>
2024-01-30 12:42:32 +0000

    Add heroku app to ALLOWED_HOSTS in settings.py

--- webpage/settings.py
+++ webpage/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,8 +24,8 @@ SECRET_KEY = "django-insecure-z93nc61!^9yfltx1b)!wuiy9#7nahwi-@qn#6p_0fdezy=fygm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jammyjobber-simplewebapp-977d4fb51203.herokuapp.com','198.211.99.20', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['jammy-jobber-0a37e91b6b51.herokuapp.com', 'jammyjobber-simplewebapp-977d4fb51203.herokuapp.com',
+                 '198.211.99.20', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "webpage.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -80,7 +78,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -100,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -111,7 +107,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
