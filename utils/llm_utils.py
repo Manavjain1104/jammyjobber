301eeec0f1abe75bdfa0e66d449f7e49942747dd
christieszi <kristi060802@gmail.com>
2024-02-02 11:28:35 +0000

    Add question answering model and another summarisation model

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -2,21 +2,33 @@ import os
 import requests
 import json
 from enum import Enum
+from huggingface_hub import InferenceClient
 
 ADDRESS = os.getenv('LLM_SERVER_ADDRESS')
+TOKEN = os.getenv('INFERENCE_API_TOKEN')
 HEADERS = {'Content-Type': 'application/json'}
+ANSWERER_MODEL = 'deepset/roberta-base-squad2'
+FACEBOOk_MODEL = 'facebook/bart-large-cnn'
 
 class Model(Enum):
     SUMMARISER = 1
     EXTRACTOR_REQUEST = 2
     EXTRACTOR_DESCRIPTION = 3
-    NONE = 4
+    FACEBOOK_SUMMARISER = 4
+    NONE = 5
 
 def process_data(text, model):
     if model == Model.SUMMARISER:
         return create_embedding(create_summary(text))
+    elif model == Model.EXTRACTOR_DESCRIPTION:
+        #768 vector model
+        return create_embedding(get_job_details(text)) + create_embedding(get_skills_required(text))
     elif model == Model.EXTRACTOR_REQUEST:
-        return create_embedding(get_suggested_job(text)) + create_embedding(get_canidate_skills(text))
+        return create_embedding(get_suggested_job(text)) + create_embedding(get_candidate_skills(text))
+    elif model == Model.FACEBOOK_SUMMARISER:
+        return create_embedding(create_summary_facebook_model(text))
+    elif model == Model.NONE:
+        return create_embedding(text)
     else:
         raise Exception("Unknown model")
 
@@ -30,26 +42,46 @@ def create_summary(text: str) -> str:
     else:
         raise Exception(f"Error: {summary_response.status_code}, {summary_response.json()}")
 
+
+def create_summary_facebook_model(text):
+    client = InferenceClient(token=TOKEN,
+                             model=FACEBOOk_MODEL)
+    answer = client.summarization(text)
+    return answer
+
 # create one vector embedding given a description in form of a string
 # example: create_embedding("some job")
 
-def get_canidate_skills(request):
-    json_single_data = json.dumps(request)
-    summary_response = requests.post(ADDRESS + "get_candidate_skills", data=json_single_data, headers=HEADERS)
-    if summary_response.status_code == 200:
-        summary = summary_response.json()['answer']
-        return summary
-    else:
-        raise Exception(f"Error: {summary_response.status_code}, {summary_response.json()}")
+def get_skills_required(description):
+    client = InferenceClient(token=TOKEN,
+                             model=ANSWERER_MODEL)
+    answer = client.question_answering(question="What skills are required?",
+                              context=description)['answer']
+    return answer
+
+
+def get_job_details(description):
+    client = InferenceClient(token=TOKEN,
+                             model=ANSWERER_MODEL)
+    answer = client.question_answering(question="What are the job details?",
+                              context=description)['answer']
+    return answer
+
+def get_candidate_skills(request):
+    client = InferenceClient(token=TOKEN,
+                             model=ANSWERER_MODEL)
+    answer = client.question_answering(question="What skills do they have?",
+                              context=request)['answer']
+    return answer
+
 
 def get_suggested_job(request):
-    json_single_data = json.dumps(request)
-    summary_response = requests.post(ADDRESS + "get_suggested_job", data=json_single_data, headers=HEADERS)
-    if summary_response.status_code == 200:
-        summary = summary_response.json()['answer']
-        return summary
-    else:
-        raise Exception(f"Error: {summary_response.status_code}, {summary_response.json()}")
+    client = InferenceClient(token=TOKEN,
+                             model=ANSWERER_MODEL)
+    answer = client.question_answering(question="What should they work as?",
+                              context=request)['answer']
+    return answer
+
 
 def create_embedding(description):
     json_single_data = json.dumps(description)
