f5348284a1a345716d2367ed8e833fb4b62f99ce
christieszi <kristi060802@gmail.com>
2024-02-02 11:58:03 +0000

    Add code to populate database

--- utils/example_embeddings_and_semadb.py
+++ utils/example_embeddings_and_semadb.py
@@ -1,10 +1,11 @@
 from semadb_utils import *
 from llm_utils import *
+from sqlite_utils import *
 
 # create_collection("testJobs", 384, "cosine")
-jobs = ["This is a Software Engineering job.", "You will be a receptionist at our clinic.",
-        "We are looking for a python developer. We can offer a competetive salary", "Charity worker needed.",
-        "Dog sitter job in Manchester"]
+# jobs = ["This is a Software Engineering job.", "You will be a receptionist at our clinic.",
+        # "We are looking for a python developer. We can offer a competetive salary", "Charity worker needed.",
+       #  "Dog sitter job in Manchester"]
 # embedding_1 = create_embedding(jobs[0])
 # embedding_2 = create_embedding(jobs[1])
 # add_points("testJobs", [new_point(embedding_1, 0),
@@ -25,5 +26,22 @@ jobs = ["This is a Software Engineering job.", "You will be a receptionist at ou
 
 # print(create_collection(COLLECTION_NAME, 384))
 
+# print(create_collection("JobAnswerer", 768))
+# print(create_collection("JobFacebook", 384))
 
-print(process_data("My name is Clara. I live in London one of the most beautiful cities", Model.FACEBOOK_SUMMARISER))
+
+# connection = sqlite3.connect(job_listing_db, check_same_thread=False)
+# cursor = connection.cursor()
+# jobs = read_job_listings(connection)
+
+# Assuming Job model has title and description fields
+# vectors = []
+# ids = []
+# for job in jobs:
+#         vectors.append(process_data(job[4], Model.EXTRACTOR_DESCRIPTION))
+#         ids.append(job[0])
+#
+# if (len(vectors) == 110 and len(ids) == 110):
+#         bulk_add_points(COLLECTION_ANSWERER_NAME, vectors, ids)
+
+print(get_collection(COLLECTION_ANSWERER_NAME))
