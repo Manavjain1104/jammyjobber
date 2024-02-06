3ede45a04e3ca50480bd174a7397600c757f096b
Zimina, Kristina <kristina.zimina21@imperial.ac.uk>
2024-02-06 12:16:08 +0000

    Update settings.py by adding csrf trusted origins

--- webpage/settings.py
+++ webpage/settings.py
@@ -119,3 +119,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ['https://jammy-jobber-0a37e91b6b51.herokuapp.com/']
