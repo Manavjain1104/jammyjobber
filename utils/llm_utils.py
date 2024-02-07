e81b11ca63f4484e32a478faf867084e0239d648
Zimina, Kristina <kristina.zimina21@imperial.ac.uk>
2024-02-07 15:45:39 +0000

    Remove bad dependencies

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -2,10 +2,6 @@ import os
 import requests
 import json
 from enum import Enum
-from huggingface_hub import InferenceClient
-import numpy as np
-from enum import Enum
-from huggingface_hub import InferenceClient
 import numpy as np
 
 ADDRESS = os.getenv('LLM_SERVER_ADDRESS')
@@ -67,39 +63,15 @@ def process_data(text, model):
     else:
         raise Exception("Unknown model")
 
-# Do locql summary and embedding
-DEV_LOCAL = True
-
-if DEV_LOCAL:
-    from transformers import pipeline
-    from sentence_transformers import SentenceTransformer
-    import numpy as np
-    from typing import List
-
-    MAX_LEN = 1000
-    MIN_LEN = 30
-
-    summariser = pipeline(
-        "summarization", model="Falconsai/text_summarization")
-    embedder = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2')
-
 
 def create_summary(text: str) -> str:
-    if DEV_LOCAL:
-        summariser_output = summariser(
-            text, max_length=MAX_LEN, min_length=MIN_LEN, do_sample=False
-        )
-        summary = summariser_output[0]['summary_text']
-        return summary
-
     json_single_data = json.dumps(text)
     summary_response = requests.post(ADDRESS + "get_summary", data=json_single_data, headers=HEADERS)
     if summary_response.status_code == 200:
         summary = summary_response.json()['summary']
         return summary
     else:
-        raise Exception(
-            f"Error: {summary_response.status_code}, {summary_response.json()}")
+        raise Exception(f"Error: {summary_response.status_code}, {summary_response.json()}")
 
 
 # def create_summary_facebook_model(text):
@@ -153,37 +125,22 @@ def get_suggested_job(request):
 
 
 def create_embedding(description):
-    if DEV_LOCAL:
-        embedding = embedder.encode([description])
-        embedding_normalised = embedding / \
-            np.linalg.norm(embedding, axis=1, keepdims=True)
-        return embedding_normalised[0].tolist()
-
     json_single_data = json.dumps(description)
-    embedding_response = requests.post(
-        ADDRESS + "get_embedding", data=json_single_data, headers=HEADERS)
+    embedding_response = requests.post(ADDRESS + "get_embedding", data=json_single_data, headers=HEADERS)
     if embedding_response.status_code == 200:
         embedding = embedding_response.json()['embedding']
         return embedding
     else:
-        raise Exception(
-            f"Error: {embedding_response.status_code}, {embedding_response.json()}")
+        raise Exception(f"Error: {embedding_response.status_code}, {embedding_response.json()}")
 
 
 # create a list of vector embeddings given a list of descriptions
 def bulk_create_embeddings(descriptions):
-    if DEV_LOCAL:
-        embeddings = embedder.encode(descriptions)
-        embeddings_normalised = embeddings / \
-            np.linalg.norm(embeddings, axis=1, keepdims=True)
-        return list(map(lambda e: e.tolist(), embeddings_normalised))
-
     json_many_data = json.dumps(descriptions)
-    bulk_embedding_response = requests.post(
-        ADDRESS + "get_embeddings", data=json_many_data, headers=HEADERS)
+    bulk_embedding_response = requests.post(ADDRESS + "get_embeddings", data=json_many_data, headers=HEADERS)
     if bulk_embedding_response.status_code == 200:
         bulk_embedding = bulk_embedding_response.json()['embeddings']
         return bulk_embedding
     else:
-        raise Exception(
-            f"Error: {bulk_embedding_response.status_code}, {bulk_embedding_response.json()}")
+        raise Exception(f"Error: {bulk_embedding_response.status_code}, {bulk_embedding_response.json()}")
+
