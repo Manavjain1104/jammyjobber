37cabea59dc76d14bb5c0329437f2179d6a821e1
Krish Maha <krish@mahas.net>
2024-02-13 02:27:21 +0000

    Implement initial changes to frontend

--- pages/static/js/job_listing.js
+++ pages/static/js/job_listing.js
@@ -1,3 +1,11 @@
 function toggleDescription(button, jobId) {
-  $(button).siblings(`#description-${jobId}`).toggle();
+  // $(button).siblings(`#description-${jobId}`).toggle();
+  var descriptionElement = document.getElementById('description-' + jobId);
+    if (descriptionElement.style.display === 'none' || descriptionElement.style.display === '') {
+        descriptionElement.style.display = 'block';
+        descriptionElement.classList.add('active-description'); // Add class when shown
+    } else {
+        descriptionElement.style.display = 'none';
+        descriptionElement.classList.remove('active-description'); // Remove class when hidden
+    }
 }
